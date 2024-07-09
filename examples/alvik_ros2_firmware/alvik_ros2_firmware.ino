5994971883543c31f5a06149926835e4362bdd3f
Alexander Entinger <consulting@lxrobotics.com>
2024-07-09 13:23:31 +0200

    Publish (currently empty, but for basic timestamp) nav_msgs/Odometry.

--- examples/alvik_ros2_firmware/alvik_ros2_firmware.ino
+++ examples/alvik_ros2_firmware/alvik_ros2_firmware.ino
@@ -19,6 +19,7 @@
 
 #include <std_msgs/msg/int32.h>
 
+#include <nav_msgs/msg/odometry.h>
 #include <geometry_msgs/msg/twist.h>
 
 #include "wifi_secrets.h"
@@ -28,13 +29,12 @@ rcl_subscription_t cmd_vel_sub;
 geometry_msgs__msg__Twist cmd_vel_msg;
 
 rcl_timer_t odom_timer;
-
-//rcl_publisher_t publisher;
-//std_msgs__msg__Int32 msg;
+rcl_publisher_t odom_pub;
+nav_msgs__msg__Odometry odom_msg;
 
 rclc_support_t support;
 rcl_allocator_t allocator;
-rclc_executor_t executor;
+rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
 rcl_node_t node;
 
 Arduino_Alvik alvik;
@@ -76,7 +76,13 @@ void odom_timer_callback(rcl_timer_t * timer, int64_t last_call_time)
   RCLC_UNUSED(last_call_time);
   if (timer != NULL)
   {
-    Serial.println(millis());
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    odom_msg.header.stamp.sec = ts.tv_sec;
+    odom_msg.header.stamp.nanosec = ts.tv_nsec;
+
+    if (rcl_ret_t const rc = rcl_publish(&odom_pub, &odom_msg, NULL); rc != RCL_RET_OK)
+      error_loop("odom_timer_callback::rcl_publish failed with %d", rc);
   }
 }
 
@@ -97,48 +103,31 @@ void setup()
   if (rcl_ret_t const rc = rclc_support_init(&support, 0, NULL, &allocator); rc != RCL_RET_OK)
     error_loop("rclc_support_init failed with %d", rc);
 
-  if (rcl_ret_t const rc = rclc_node_init_default(&node, "alvik_node", "", &support); rc != RCL_RET_OK)
+  if (rcl_ret_t const rc = rclc_node_init_default(&node, "alvik", "", &support); rc != RCL_RET_OK)
     error_loop("rclc_node_init_default failed with %d", rc);
-
-  // create publisher
-//  rc = rclc_publisher_init_best_effort(&publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "topic_name");
-//  if (rc != RCL_RET_OK)
-//  {
-//    char msg[64] = {0};
-//    snprintf(msg, sizeof(msg), "rclc_publisher_init_best_effort failed with %d", rc);
-//    Serial.println(msg);
-//    error_loop();
-//  }
 
   if (rcl_ret_t const rc = rclc_subscription_init_default(&cmd_vel_sub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist), "/cmd_vel"); rc != RCL_RET_OK)
     error_loop("rclc_subscription_init_default failed with %d", rc);
 
-  if (rcl_ret_t const rc = rclc_timer_init_default(&odom_timer, &support, RCL_MS_TO_NS(10), odom_timer_callback); rc != RCL_RET_OK)
-    error_loop("rclc_timer_init_default failed with %d", rc);
+  if (rcl_ret_t const rc = rclc_timer_init_default(&odom_timer, &support, RCL_MS_TO_NS(50), odom_timer_callback); rc != RCL_RET_OK)
+    error_loop("rclc_timer_init_default(odom_timer, ...) failed with %d", rc);
+
+  if (rcl_ret_t const rc = rclc_publisher_init_default(&odom_pub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(nav_msgs, msg, Odometry), "/odom"); rc != RCL_RET_OK)
+    error_loop("rclc_publisher_init_default(odom_pub, ...) failed with %d", rc);
 
   if (rcl_ret_t const rc = rclc_executor_init(&executor, &support.context, 2, &allocator); rc != RCL_RET_OK)
     error_loop("rclc_executor_init failed with %d", rc);
 
   if (rcl_ret_t const rc = rclc_executor_add_subscription(&executor, &cmd_vel_sub, &cmd_vel_msg, &cmd_vel_callback, ON_NEW_DATA); rc != RCL_RET_OK)
-    error_loop("rclc_executor_add_subscription failed with %d", rc);
+    error_loop("rclc_executor_add_subscription(..., cmd_vel_sub, ...) failed with %d", rc);
 
   if (rcl_ret_t const rc = rclc_executor_add_timer(&executor, &odom_timer); rc != RCL_RET_OK)
-    error_loop("rclc_executor_add_timer failed with %d", rc);
+    error_loop("rclc_executor_add_timer(..., odom_timer, ...) failed with %d", rc);
 
   Serial.println("alvik_ros2_firmware setup complete.");
 }
 
 void loop()
 {
-//  static auto prev = millis();
-//  auto const now = millis();
-//
-//  if ((now - prev) > 1000)
-//  {
-//    prev = now;
-//    Serial.println(msg.data);
-//    rcl_publish(&publisher, &msg, NULL);
-//    msg.data++;
-//  }
   rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
 }
