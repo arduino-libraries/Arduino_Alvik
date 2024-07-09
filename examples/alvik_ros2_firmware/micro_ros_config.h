9f8828dc430086fe1b328a086b6cfe08d0839758
Alexander Entinger <consulting@lxrobotics.com>
2024-07-09 10:23:59 +0200

    Extract Micros-ROS configuration in separate_file "micro_ros_config.h".

---
+++ examples/alvik_ros2_firmware/micro_ros_config.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2024 Arduino
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#define MICRO_ROS_AGENT_IP "YOUR_MICRO_ROS_AGENT_IP"
+#define MICRO_ROS_AGENT_PORT 8888
